faa1e50f685c772f2f3800bb8d93c3eb25df37a4
baij1314 <35292770+baij1314@users.noreply.github.com>
2018-09-05 17:14:34 +0800

    Update atou_mysql_data.sh

--- atou_mysql_data.sh
+++ atou_mysql_data.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-echo -n  "please mysql password  ->"
-read -s  PASSWD
-MYUSER=root
-MYPASS=$PASSWD
-SOCKET=/www/mysql/mysql.sock
-MYBAKDIR=/www/backup
-MYLOGIN="mysql -u$MYUSER -p$MYPASS -S $SOCKET"
-MYDUMP="mysqldump -u$MYUSER -p$MYPASS -S$SOCKET -B"
-SHOW=$MYLOGIN -e "show databases;"
-DATABASE="$($MYLOGIN -e "show databases;"|egrep -vi "Data|_schema|mysql")"
-echo $DATABASE 
-for dbname in $DATABASE
-  do
-   MYDIR=/$MYBAKDIR/$dbname
-   [ ! -d $MYDIR ] && mkdir -p $MYDIR
- $MYDUMP $dbname|gzip >$MYDIR/${dbname}_$(date +%F).sql.gz
-done
-ls -l  $MYBAKDIR
+#mysql backup
+find  /www/backup/ -name '*.sql'   -type f -mtime  +30 -exec rm -rf {} \;
+MYUSER=script_user
+export MYSQL_PWD=123123
+mysql -e "show databases;" -u$MYUSER -p$MYSQL_PWD | egrep -vi "Data|_schema|mysql|test|job|order" | xargs mysqldump -u$MYUSER -p$MYSQL_PWD --databases > /www/backup/mysql_$(date +%F)_dump.all.sql
+
+#dell 30 day   mpurse logs  
+find  /opt/tomcat/  -name '*.log'    -type f -mtime  +30 -exec rm -rf {} \;
+find  /opt/tomcat/ -name '*.log.*'   -type f -mtime  +15 -exec rm -rf {} \;
+
+#MYUSER=root
+#MYSQL_PWD=123123
+#SOCKET=/var/lib/mysql/mysql.sock
+#MYBAKDIR=/www/backup/mysql$(date +%F)
+#MYLOGIN="mysql -u$MYUSER -p$MYSQL_PWD -S $SOCKET"
+#MYDUMP="mysqldump -u$MYUSER -p$MYSQL_PWD -S$SOCKET -B"
+#SHOW=$MYLOGIN -e "show databases;"
+#DATABASE="$($MYLOGIN -e "show databases;"|egrep -vi "Data|_schema|mysql")"
+#for dbname in $DATABASE
+#  do
+#   MYDIR=/$MYBAKDIR/$dbname
+#   [ ! -d $MYDIR ] && mkdir -p $MYDIR
+# $MYDUMP $dbname >$MYDIR/${dbname}.sql
+#done
